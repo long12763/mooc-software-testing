d3f7a89f1240cf98ba1bb4d90f12e425d291a60f
Long <abc.cba12764@gmail.com>
2020-11-14 23:00:06 +0700

    complete the test class for addFive

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,7 +1,25 @@
 package tudelft.gettingstarted;
 
+import org.graalvm.compiler.debug.Assertions;
+
 public class GettingStarted {
     public int addFive (int number) {
         return number + 5;
+    }
+}
+public void addFiveToTwenty() {
+        int result = new GettingStarted().addFive(20);
+        Assertions.assertEquals(25,result);
+    }
+// Add the correct numbers
+public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+// Write this test
+public void addFiveToMinusTwenty() {
+
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15, result);
     }
 }
